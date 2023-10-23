01943280444a4393002e28a292cf127cf71df08d
ozi <ozisadiqkasai@gmail.com>
2023-10-23 13:47:14 +0100

    modified:   src/components/Sidebar.js

--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -6,12 +6,13 @@ const Sidebar = () => {
  const{handleClose} = useContext(SidebarContext)
  return (
    <div className="h-full w-full bg-white fixed top-0 right-0 md:w-[35vw] xl:max-w-[30vw] transition-all duration-300 z-20 px-4 lg:px-[35px] opacity-90">
-     <div className="border-b flex items-center justify-between">
+     <div className=" container border-b flex items-center justify-between">
        <p className="uppercase text-sm font-semibold py-2">Shopping Bag(0)</p>
        <div className="cursor-pointer" onClick={handleClose}>
          <HiArrowLongRight />
        </div>
      </div>
+     cartitem
    </div>
    // <div onClick={handleClose}>sidebar</div>
  )
