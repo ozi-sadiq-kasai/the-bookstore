8130e5c4e4124bf7e0c4c037c8a7e712c1dbae0f
ozi <ozisadiqkasai@gmail.com>
2023-10-25 08:35:04 +0100

    modified files

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -1,10 +1,10 @@
 const Hero = () => {
   return (
-    <div className="">
+    <div className="w-full">
       <img
         src={'src/assets/images/background0.jpg'}
         alt="books background image"
-        className='object-cover md:w-full h-80'
+        className='object-cover w-full h-80'
       />
     </div>
   )
